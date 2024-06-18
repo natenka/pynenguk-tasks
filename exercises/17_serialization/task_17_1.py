2cbb7f445de73e4b107b327aa524ccb221eab4bc
Natasha Samoylenko <natasha.samoylenko@gmail.com>
2024-06-18 12:39:46 +0300

    Update task_17_1.py

--- exercises/17_serialization/task_17_1.py
+++ exercises/17_serialization/task_17_1.py
@@ -20,7 +20,7 @@ MacAddress          IpAddress        Lease(sec)  Type           VLAN  Interface
 00:E9:22:11:A6:50   100.1.1.7        76260       dhcp-snooping   3    FastEthernet0/21
 Total number of bindings: 2
 
-У підсумковому csv файлі має бути такий вміст:
+У кінцевому csv файлі має бути такий вміст:
 switch,mac,ip,vlan,interface
 sw3,00:E9:BC:3F:A6:50,100.1.1.6,3,FastEthernet0/20
 sw3,00:E9:22:11:A6:50,100.1.1.7,3,FastEthernet0/21
